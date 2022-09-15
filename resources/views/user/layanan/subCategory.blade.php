6b1174877ca5918a3732bf8a86b97a946b34fd6d
Teguhriyadi <ilham.teguh55@gmail.com>
2022-09-15 22:37:31 +0700

    PUSH

--- resources/views/user/layanan/subCategory.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach($subcategories as $subcategory)
- <ul>
-    <li>{{$subcategory->name}}</li> 
-  @if(count($subcategory->subcategory))
-    @include('product.subCategoryList',['subcategories' => $subcategory->subcategory])
-  @endif
- </ul> 
-@endforeach
